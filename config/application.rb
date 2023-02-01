bdc161c546423de9999d1edbdc6d53f1a3e0739a
Paul martins <paul@opslevel.com>
2023-01-31 21:49:45 -0500

    Rename project to Updraft

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Updraftv2
+module Updraft
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
