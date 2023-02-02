68faf6219f51a25461a00c72be83bc9133138f75
Paul martins <paul@opslevel.com>
2023-02-01 22:46:53 -0500

    Refactor rake task to use Interactors

---
+++ app/interactors/download_bulk_data.rb
@@ -0,0 +1,33 @@
+require './lib/scryfall/bulk_data.rb'
+require 'open-uri'
+
+class DownloadBulkData
+  include Troupe
+
+  def call
+    download_cards
+    persist_cards
+  end
+
+  def download_cards
+    puts "Fetching newest bulk exports..."
+    response = Scryfall::BulkData.type("oracle_cards")
+    puts "Found a bulk export from #{response.to_h.dig(:updated_at)}"
+    puts "Downloading..."
+
+    download_uri = response.to_h.dig(:download_uri)
+    download = URI.open(download_uri)
+
+    IO.copy_stream(download, 'tmp/import.json')
+  end
+
+  def persist_cards
+    puts "Download complete, ingesting cards into database..."
+    json_data = File.read("tmp/import.json")
+    parsed_cards = JSON.parse(json_data)
+
+    parsed_cards.each do |c|
+      Card.new(name: c["name"], scryfall_id: c["id"], oracle_text: c["oracle_text"]).save!
+    end
+  end
+end
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -1,28 +1,6 @@
-require './lib/scryfall/bulk_data.rb'
-
 namespace :import do
   desc "Import the newest Scryfall Bulk Data set into the database"
   task bulk_data: [:environment] do
-    puts "Fetching newest bulk exports..."
-    response = Scryfall::BulkData.type("oracle_cards")
-    puts "Found a bulk export from #{response.to_h.dig(:updated_at)}"
-    puts "Downloading..."
-
-    download_uri = response.to_h.dig(:download_uri)
-    download = URI.open(download_uri)
-
-    IO.copy_stream(download, 'tmp/import.json')
-
-    persist
-  end
-end
-
-
-def persist
-  json_data = File.read("tmp/import.json")
-  parsed_cards = JSON.parse(json_data)
-
-  parsed_cards.each do |c|
-    Card.new(name: c["name"], scryfall_id: c["id"], oracle_text: c["oracle_text"]).save!
+    DownloadBulkData.call()
   end
 end
